69c09888c72e56befb4cb77320bc20fb4494b8f6
unknown <antonatoac@gmail.com>
2020-03-16 13:16:56 -0300

    ajax json

---
+++ AJAX Json/ajax1.js
@@ -0,0 +1,35 @@
+// console.log('activo');
+
+document.querySelector('#boton').addEventListener('click', traerDatos);
+
+function traerDatos(){
+
+    // console.log('dentro de la función');
+    
+    const xhttp = new XMLHttpRequest;
+
+    xhttp.open('GET', 'gatos.json', true);
+    xhttp.send();
+
+    xhttp.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            // console.log(this.responseText); 
+            let datos = JSON.parse(this.responseText);
+            console.log(datos);
+
+            let res = document.querySelector('#res');
+            res.innerHTML = '';
+
+            for(let item of datos){
+                console.log(item.titulo);
+                res.innerHTML += 
+                `
+                <tr>
+                    <td>${item.titulo}</td>
+                    <td>${item.Color}</td>
+                </tr>
+                `
+            }
+        }
+    }
+}
